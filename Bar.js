3c817ae9418216144c3f61a80550f05583b1d914
Vladimir <vovik261096@mail.ru>
2022-04-15 15:44:00 +0700

    com

---
+++ Bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Bar = () => {
+    return (<div>
+
+    </div>)
+}
+
+export default Bar;
---
+++ Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (<div>
+        <img src="https://img2.freepng.ru/20180604/bup/kisspng-smiley-emoticon-clip-art-funny-kids-5b1538aa9150f3.8462367215281174185952.jpg" alt="smile"></img>
+    </div>)
+}
+
+export default Header;
